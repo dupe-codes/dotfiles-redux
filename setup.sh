596c25e2b574a9d674d7e817bfe8baed8ca2ecea
Nick Dupoux <ndupoux@attentivemobile.com>
2023-04-07 19:33:47 -0400

    feat: add setup file

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
+# Install neovim
+brew install neovim
+
 # Install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # symlink config files
-ln -sf ./dotfiles-redux/vimrc.vim $HOME/.config/nvim/vimrc.vim
-ln -sf ./dotfiles-redux/init.lua $HOME/.config/nvim/init.lua
+mkdir -p $HOME/.config/nvim
+
+ln -sf ./vimrc.vim $HOME/.config/nvim/vimrc.vim
+ln -sf ./init.lua $HOME/.config/nvim/init.lua
 
