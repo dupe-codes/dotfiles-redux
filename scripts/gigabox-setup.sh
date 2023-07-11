519586819622a7e733c9c8beed5ebda24642996b
dupe <njdupe@gmail.com>
2023-07-10 23:32:43 -0400

    update gigabox script to unzip resources in home dir

--- scripts/gigabox-setup.sh
+++ scripts/gigabox-setup.sh
@@ -1,14 +1,15 @@
 # Download yay package manager
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si
+#git clone https://aur.archlinux.org/yay-bin.git
+#cd yay-bin
+#makepkg -si
 
 # TODO: download and install packages
 
 # Download resources (wallpapers, fonts, etc.) from dropbox
-wget -O $PWD/gigabox/gigabox-resources.zip 'https://www.dropbox.com/sh/zff0e94426lpj95/AABiFP2pxBtgd-VgQRekbR33a?dl=0'
-unzip $PWD/gigabox/gigabox-resources.zip
+mkdir -p $HOME/gigabox
+wget -O $HOME/gigabox/resources.zip 'https://www.dropbox.com/sh/zff0e94426lpj95/AABiFP2pxBtgd-VgQRekbR33a?dl=0'
+unzip $HOME/gigabox/resources.zip -d $HOME/gigabox/
 
 # Enable services
-sudo systemctl enable lightdm.service
+#sudo systemctl enable lightdm.service
 
