6f8f5dee9f60d6053f0a3ff71159e19a307d4d33
dupe <njdupe@gmail.com>
2023-07-11 00:39:49 -0400

    note: 📝 todos in setup script

--- scripts/gigabox-setup.sh
+++ scripts/gigabox-setup.sh
@@ -1,7 +1,7 @@
 # Download yay package manager
-#git clone https://aur.archlinux.org/yay-bin.git
-#cd yay-bin
-#makepkg -si
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
 
 # TODO: download and install packages
 
@@ -11,8 +11,12 @@ wget -O $HOME/gigabox/resources.zip 'https://www.dropbox.com/sh/zff0e94426lpj95/
 unzip $HOME/gigabox/resources.zip -d $HOME/gigabox/
 
 # TODO: Link icons to /usr/share/icons and ~/.icons
+#       Need to find good icon pack options
 # TODO: Link fonts to ~/.fonts
+#       Need to find good font with icon options
+
+# TODO: Link all configuration files
 
 # Enable services
-#sudo systemctl enable lightdm.service
+sudo systemctl enable lightdm.service
 
