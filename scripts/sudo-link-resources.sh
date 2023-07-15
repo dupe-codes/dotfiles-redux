7abb7f5f5d45881bfee2b860a88eb5b375e21db3
dupe <njdupe@gmail.com>
2023-07-14 20:02:20 -0400

    fix: 🐛 cp web-greeter config instead of symlink

--- scripts/sudo-link-resources.sh
+++ scripts/sudo-link-resources.sh
@@ -2,8 +2,10 @@
 
 sudo ln -vsf $PWD/gigabox/resources/icons/Papirus-Dark-Custom /usr/share/icons
 sudo ln -vsf $PWD/gigabox/lightdm/lightdm.conf /etc/lightdm/lightdm.conf
-sudo ln -vsf $PWD/gigabox/lightdm/web-greeter.yml /etc/lightdm/web-greeter.yml
 sudo ln -vsf $PWD/gigabox/resources/wallpapers /usr/share/backgrounds
+
+# symlinking to web-greeter config doesn't work for... reasons, so we copy it
+sudo cp -v $PWD/gigabox/lightdm/web-greeter.yml /etc/lightdm/web-greeter.yml
 
 # Setup dupe user image :]
 sudo mkdir -p /var/lib/AccountsService/icons
