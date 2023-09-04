55ec0ae7c628b25d8f88c528026a4072e36e94dc
dupe <njdupe@gmail.com>
2023-09-04 14:15:17 -0400

    feat: ✨ fix up coqtail commands to grab word under cursor

--- neovim/lua/dupe/configs/coqtail.lua
+++ neovim/lua/dupe/configs/coqtail.lua
@@ -4,6 +4,31 @@ vim.cmd("autocmd BufNewFile,BufRead *.v set filetype=coq")
 
 local whichkey = require("which-key")
 
+local getcurword = function()
+    -- Add '.' to definition of a keyword
+    local old_keywd = vim.api.nvim_get_option('iskeyword')
+    vim.api.nvim_buf_set_option(0, 'iskeyword', old_keywd .. ',' .. '.')
+
+    local cword = vim.fn.expand('<cword>')
+
+    -- Strip trailing '.'s
+    local dotidx = string.find(cword, "[.]+$")
+    if dotidx then
+        cword = string.sub(cword, 1, dotidx - 1)
+    end
+
+    -- Reset iskeyword
+    vim.api.nvim_buf_set_option(0, 'iskeyword', old_keywd)
+
+    return cword
+end
+
+function execute_command_with_word(command_base)
+    local word = getcurword()
+    local cmd = string.format(command_base, word)
+    vim.cmd(cmd)
+end
+
 local keymap = {
     p = {
         name = "Proofs (Coq)",
@@ -19,11 +44,11 @@ local keymap = {
         d = { "<cmd>CoqJumpToError<CR>", "Move cursor to start of error region" },
         c = {
             name = "Queries",
-            h = { "<cmd>Coq Check<CR>", "Show type of <arg>" },
-            a = { "<cmd>Coq About<CR>", "Show info about <arg>" },
-            p = { "<cmd>Coq Print<CR>", "Show definition of <arg>" },
-            d = { "<cmd>Coq Locate<CR>", "Show location of <arg>" },
-            s = { "<cmd>Coq Search<CR>", "Search for theorems about <arg>" },
+            h = { ":lua execute_command_with_word('Coq Check %s')<CR>", "Show type of <arg>" },
+            a = { ":lua execute_command_with_word('Coq About %s')<CR>", "Show info about <arg>" },
+            p = { ":lua execute_command_with_word('Coq Print %s')<CR>", "Show definition of <arg>" },
+            d = { ":lua execute_command_with_word('Coq Locate %s')<CR>", "Show location of <arg>" },
+            s = { ":lua execute_command_with_word('Coq Search %s')<CR>", "Search for theorems about <arg>" },
         },
         r = { "<cmd>CoqRestorePanels<CR>", "Restore coq panels" },
     }
