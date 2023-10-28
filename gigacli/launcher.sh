840f69a57b0663605d66493088bcda4dc2b2c01b
dupe <njdupe@gmail.com>
2023-10-28 13:54:29 -0400

    fix: 🐛 switch to gum filter in launcher script

--- gigacli/launcher.sh
+++ gigacli/launcher.sh
@@ -110,7 +110,7 @@ for tool in "${tool_order[@]}"; do
     prompt_list+="$line"
 done
 
-selected_tool=$(echo -e "$prompt_list" | gum choose --height 20)
+selected_tool=$(echo -e "$prompt_list" | gum filter --height 20)
 tool=$(echo "$selected_tool" | cut -d ' ' -f 1)
 
 if [ -z "$tool" ]; then
