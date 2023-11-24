4028f20a1c9ac063c6cf1c41589878b1d67220d8
dupe <njdupe@gmail.com>
2023-11-24 17:39:48 -0500

    fix: 🐛 improved keybind separator for cheatsheets

--- gigabox/polybar/blocks/scripts/cheatsheets.sh
+++ gigabox/polybar/blocks/scripts/cheatsheets.sh
@@ -51,7 +51,7 @@ for line in "${lines[@]}"; do
     fi
 
     if [ "$is_description_section" = false ]; then
-        key=$(echo $line | cut -d';' -f1)
+        key=$(echo $line | awk -F ':=' '{print $1}')
         length=${#key}
         if (( length > max_length )); then
             max_length=$length
@@ -64,8 +64,8 @@ done
 
 formatted=""
 for line in "${keybinds[@]}"; do
-    short_description=$(echo $line | cut -d';' -f1)
-    keybind=$(echo $line | cut -d';' -f2)
+    short_description=$(echo $line | awk -F ':=' '{print $1}')
+    keybind=$(echo $line | awk -F ':=' '{print $2}')
     padded_description=$(printf "%-${max_length}s" "$short_description")
     formatted+="$padded_description => $keybind\n"
 done
@@ -74,11 +74,11 @@ done
 rofi_command="rofi -no-config -theme $dir/keybinds.rasi"
 selected_keybind=$(echo -e "$formatted" | $rofi_command -dmenu -p "$sheet_name")
 
-selected_key=$(echo $selected_keybind | cut -d'=' -f1 | xargs)
+selected_key=$(echo $selected_keybind | awk -F '=>' '{print $1}' | xargs)
 for line in "${descriptions[@]}"; do
-    key=$(echo $line | cut -d ';' -f1 | xargs)
+    key=$(echo $line | awk -F ':=' '{print $1}' | xargs)
     if [[ $selected_key == "$key" ]]; then
-        description=$(echo $line | cut -d ';' -f2)
+        description=$(echo $line | awk -F ':=' '{print $2}')
         wrapped_description=$(wrap_text "$description" $LINE_BREAK_THRESHOLD)
         echo -e "$wrapped_description" | $rofi_command -theme $dir/keybinds.rasi -dmenu -p "$key"
         break
