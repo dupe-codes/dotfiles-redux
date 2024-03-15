bf8e83c607c7d26faa067498c701d7cd88d3f1ec
dupe <dupe@dupe.codes>
2024-03-14 23:17:44 -0400

    assorted cleanups

--- posix-cli/.wezterm.lua
+++ posix-cli/.wezterm.lua
@@ -6,6 +6,7 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
+config.leader = { key = 'a', mods = 'SUPER', timeout_milliseconds = 1000 }
 config.font = wezterm.font_with_fallback { 'MonoLisa Custom', 'Noto Sans Symbols' }
 config.color_scheme = 'Tokyo Night'
 config.window_background_opacity = 0.95
@@ -14,5 +15,6 @@ config.cursor_blink_rate = 650
 config.hide_tab_bar_if_only_one_tab = true
 config.animation_fps = 1
 config.default_prog = { '/usr/bin/zsh' }
+config.audible_bell = 'Disabled'
 
 return config
