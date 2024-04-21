40a167f2f6f11be4dff7c4cb2321ecec397487de
dupe <dupe@dupe.codes>
2024-04-21 16:01:07 -0400

    chore: 🔧 remove migrated scripts and files

--- gigabox/polybar/blocks/scripts/clipboard.sh
+++
@@ -1,4 +0,0 @@
-rofi -modi "clipboard:greenclip print" \
-     -show clipboard \
-     -run-command '{cmd}' \
-     -theme ~/.config/polybar/blocks/scripts/rofi/launcher.rasi
--- gigacli/scripts/timer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-timew start "$@";
-echo ''
-gum spin --spinner dot --title "Time tracking 󰄉" -- sleep 10800
-echo ''
-timew stop
