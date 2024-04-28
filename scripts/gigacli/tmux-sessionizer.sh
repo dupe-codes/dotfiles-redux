e2725d04de634c70e3536307537d1cbaca9d4804
dupe <dupe@dupe.codes>
2024-04-28 14:58:26 -0400

    feat(gigacli): ✨ init commands for tmux-sessionizer

--- scripts/gigacli/tmux-sessionizer.sh
+++ scripts/gigacli/tmux-sessionizer.sh
@@ -2,6 +2,13 @@
 
 # Adapted from ThePrimeagen (ty!!)
 # https://github.com/ThePrimeagen/.dotfiles/blob/master/bin/.local/scripts/tmux-sessionizer
+#
+# The sessionizer facilitates the creation of and connection to tmux sessions
+# linked to target project directories. In addition to this base functionality,
+# I've added support for:
+#   1. Creation of named windows based on my preferred programming setup
+#   2. Initialization commands for specific project directories, e.g. configuring
+#      opam switches for OCaml projects
 
 if [[ $# -eq 1 ]]; then
     selected=$1
@@ -25,20 +32,53 @@ else
     windows=("code" "terminal A" "terminal B")
 fi
 
+# criteria and init commands mapping
+# NOTE: for now, all commands must have two criteria to OR check on,
+#       the existence of a target dir or target file, and one command to
+#       execute when critera is met
+declare -A init_commands
+init_commands[ocaml]="$selected/_opam;$selected/.opam-switch;opam switch && eval \$(opam env)"
+
+function get_init_commands {
+    for key in "${!init_commands[@]}"; do
+        IFS=';' read -ra ADDR <<< "${init_commands[$key]}"
+        if [[ -d "${ADDR[0]}" ]] || [[ -f "${ADDR[1]}" ]]; then
+            echo "${ADDR[2]}"
+            return
+        fi
+    done
+}
+
+init_command=$(get_init_commands)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
     tmux new-session -ds $selected_name -c $selected -n "${windows[0]}"
+    if [[ -n $init_command ]]; then
+        tmux send-keys -t "$selected_name:${windows[0]}" "$init_command" C-m
+    fi
+
     for window in "${windows[@]:1}"; do
         tmux new-window -t "$selected_name" -n "$window" -c "$selected"
+        if [[ -n $init_command ]]; then
+            tmux send-keys -t "$selected_name:$window" "$init_command" C-m
+        fi
     done
+
     tmux attach-session -t "$selected_name":1
     exit 0
 fi
 
 if ! tmux has-session -t=$selected_name 2> /dev/null; then
     tmux new-session -ds $selected_name -c $selected -n "${windows[0]}"
+    if [[ -n $init_command ]]; then
+        tmux send-keys -t "$selected_name:${windows[0]}" "$init_command" C-m
+    fi
+
     for window in "${windows[@]:1}"; do
         tmux new-window -t "$selected_name" -n "$window" -c "$selected"
+        if [[ -n $init_command ]]; then
+            tmux send-keys -t "$selected_name:$window" "$init_command" C-m
+        fi
     done
 fi
 
