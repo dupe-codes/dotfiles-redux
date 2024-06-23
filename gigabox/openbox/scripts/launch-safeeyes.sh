96f8ffb51911543d2581b78c47801a1c112aba1c
dupe <dupe@dupe.codes>
2024-06-23 14:17:47 -0400

    fix(safeeyes): 🐛 launching in subshell

---
+++ gigabox/openbox/scripts/launch-safeeyes.sh
@@ -0,0 +1,5 @@
+# safeeyes fails to launch in my conda environment
+# due to missing dependencies
+# so, launch it in a new subshell that will run
+# with the system python packages
+bash -c "safeeyes"
