2595438d095ba742129c70c41105fe6b32c3ab8c
dupe <dupe@dupe.codes>
2024-07-13 17:17:10 -0400

    feat(gigacli): ✨ improved project templates patterns

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -1,4 +1,9 @@
 # TODO: Make use of these constants in all scripts
-GB_SCRIPTS_DIR="$HOME/scripts/gigabox"
-GCLI_SCRIPTS_DIR="$HOME/scripts/gigacli"
-LOGS_DIR="$HOME/logs"
+
+export DOTFILES_DIR="$HOME/projects/dotfiles-redux"
+
+export GB_SCRIPTS_DIR="$HOME/scripts/gigabox"
+export GCLI_SCRIPTS_DIR="$HOME/scripts/gigacli"
+export LOGS_DIR="$HOME/logs"
+
+export TEMPLATES_DIR="$DOTFILES_DIR/project-configs/templates"
--- scripts/gigacli/projects/godot_setup.sh
+++ scripts/gigacli/projects/godot_setup.sh
@@ -4,6 +4,9 @@
 # structure
 
 source "$HOME/scripts/utils/logging.sh"
+source "$HOME/scripts/constants.sh"
+
+set -e
 
 project_name=$(gum input --placeholder "project name")
 if [ -z "$project_name" ]; then
@@ -12,14 +15,22 @@ fi
 
 project_dir=$(echo "$project_name" | tr '[:upper:]' '[:lower:]' | tr ' ' '_')
 
-# TODO: add "TEMPLATES_DIR" to constants file
-cp -r "$HOME"/projects/dotfiles-redux/project-configs/templates/godot "$(pwd)"/"$project_dir"
+cp -r "$TEMPLATES_DIR"/godot "$(pwd)"/"$project_dir"
+cp -r "$TEMPLATES_DIR"/common/. "$(pwd)"/"$project_dir"
 
-cd "$(pwd)"/"$project_dir" &&
-    sed -i "s/<project_name>/$project_name/g" project.godot &&
-    git init &&
-    git add . &&
-    git commit -m "feat: initialized project"
+cd "$(pwd)"/"$project_dir" ||
+    (print_fail "Failed to enter newly created project directory" &&
+        exit 1)
+
+# replace project name template strings
+sed -i "s/<project_name>/$project_name/g" project.godot project.env
+
+# initialize git repo
+git init
+git add .
+pre-commit install
+pre-commit install -t prepare-commit-msg
+git commit -m "feat: initialized project"
 
 echo ""
-print_info "Project $project_name created at $(pwd)/$project_dir"
+print_info "Project $project_name created at $(pwd)"
