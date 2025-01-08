d225632230bb1debaa20da7167bea22cfaeaab2d
dupe <dupe@dupe.codes>
2025-01-08 16:13:15 -0500

    fix: 🐛 longer timer sleep

--- scripts/gigacli/timer.sh
+++ scripts/gigacli/timer.sh
@@ -6,10 +6,11 @@ echo ''
 start_time=$(date +%s)
 
 gum spin --title "Time tracking 󰄉" --show-output --spinner dot -- sh -c '
+echo ""
 while true; do
     elapsed=$(($(date +%s) - '"$start_time"'))
     printf "\n%02d:%02d:%02d" $((elapsed / 3600)) $(((elapsed / 60) % 60)) $((elapsed % 60))
-    sleep 1
+    sleep 60
 done
 '
 
