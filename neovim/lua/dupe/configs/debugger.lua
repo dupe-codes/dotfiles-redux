e1584f5505da1c39751ec400ae6a40032b66fb55
dupe <dupe@dupe.codes>
2025-01-18 18:17:12 -0500

    feat(neovim): ✨ add debug config for lua

--- neovim/lua/dupe/configs/debugger.lua
+++ neovim/lua/dupe/configs/debugger.lua
@@ -237,6 +237,35 @@ dap.configurations.gdscript = {
     },
 }
 
+-- lua and LOVE
+
+dap.adapters["local-lua"] = {
+    type = "executable",
+    command = "local-lua-dbg",
+    enrich_config = function(config, on_config)
+        if not config["extensionPath"] then
+            local c = vim.deepcopy(config)
+            c.extensionPath = "/usr/lib/node_modules/local-lua-debugger-vscode/"
+            on_config(c)
+        else
+            on_config(config)
+        end
+    end,
+}
+
+dap.configurations.lua = {
+    {
+        name = "Current file (local-lua-dbg, lua)",
+        type = "local-lua",
+        request = "launch",
+        cwd = "${workspaceFolder}",
+        program = {
+            lua = "lua",
+            file = "${file}",
+        },
+    },
+}
+
 --#endregion
 
 --#region
